2a7f62ec08f42bf16c2fc48afced3baf2448ddf2
Gonzaaa <gonzaloantuna@hotmail.com>
2020-11-06 16:11:23 -0300

    Version 0.1
    Escenas con Labels para saber cual es cada una, boton de Skip de la intro hacia Menu

---
+++ Parcial Programación - N°2/Button.gd
@@ -0,0 +1,15 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first tim
+
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Menu.tscn")
+	pass # Replace with function body.
