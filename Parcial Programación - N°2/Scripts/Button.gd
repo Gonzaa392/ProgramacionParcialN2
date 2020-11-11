4c58a484a02bc9151410c23030b96b132acd0ff8
Federico <fededelgado2001@gmail.com>
2020-11-10 21:42:11 -0300

    0.3.1
    Rutas de escenas arregladas

--- Parcial Programación - N°2/Scripts/Button.gd
+++ Parcial Programación - N°2/Scripts/Button.gd
@@ -8,7 +8,7 @@ export(String) var target_scene = ""
 
 # Called when the node enters the scene tree for the first tim
 func _ready():
-	connect("pressed", self, "on_button_pressed")
+	connect("pressed", self, "on_Button_pressed")
 
 func _on_Button_pressed():
 	var err = get_tree().change_scene(target_scene)
