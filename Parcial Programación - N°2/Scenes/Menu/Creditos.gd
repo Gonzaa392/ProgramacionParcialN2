35b2349361e2f858a8a7b916c6186d480fbe33d7
Luka <lukafranco2000@hotmail.com>
2020-11-17 20:52:55 -0300

    d

--- Parcial Programación - N°2/Scenes/Menu/Creditos.gd
+++ Parcial Programación - N°2/Scenes/Menu/Creditos.gd
@@ -1,9 +1,14 @@
 extends TextureButton
 
 
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
 export(String) var target_scene = ""
 
-# Called when the node enters the scene tree for the first time
 
-func _on_Salir_pressed():
-	get_tree().quit()
+func _on_TextureButton_pressed():
+	var err = get_tree().change_scene(target_scene)
+	if err != OK:
+		print("Error al cambiar de Escena")
+		pass
