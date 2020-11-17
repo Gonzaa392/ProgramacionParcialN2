2781439d8da985b7ade698a88f0cbe215fbd8387
Luka <lukafranco2000@hotmail.com>
2020-11-17 20:27:02 -0300

    boton de salir

--- Parcial Programación - N°2/Scenes/Menu/Creditos.gd
+++ Parcial Programación - N°2/Scenes/Menu/Creditos.gd
@@ -5,9 +5,5 @@ export(String) var target_scene = ""
 
 # Called when the node enters the scene tree for the first time
 
-
-func _on_Creditos_pressed():
-	var err = get_tree().change_scene(target_scene)
-	if err != OK:
-		print("Error al cambiar de Escena")
-	pass # Replace with function body.
+func _on_Salir_pressed():
+	get_tree().quit()
