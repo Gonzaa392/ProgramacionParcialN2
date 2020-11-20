cc83fd2116ad8a105ca99ef37d50bad6fb440ae2
Federico <fededelgado2001@gmail.com>
2020-11-20 19:28:59 -0300

    0.4.10
    Las spikes ahora pasan la variable monedas a 0
    La deathzone del level 2 ahora es más grande

--- Parcial Programación - N°2/Death.gd
+++ Parcial Programación - N°2/Death.gd
@@ -1,22 +1,9 @@
 extends Node2D
 
 export(String) var level_scene = ""
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_Area2D_body_entered(body):
+	Globals.monedas = 0
 	if body.get_name() == "car":
 		var err = get_tree().change_scene(level_scene)
 		if err != OK:
--- Parcial Programación - N°2/scenes/levels/Spikes.gd
+++ Parcial Programación - N°2/scenes/levels/Spikes.gd
@@ -3,6 +3,7 @@ extends Node2D
 export(String) var level_scene = ""
 
 func _on_Area2D_body_entered(body):
+	Globals.monedas = 0
 	if body.get_name() == "car":
 		var err = get_tree().change_scene(level_scene)
 		if err != OK:
