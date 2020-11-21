53ce602dcf4cc744bacdb77af1633a7155ce1d78
Luka <lukafranco2000@hotmail.com>
2020-11-20 22:55:12 -0300

    codigo que se ve mas lindo para el profe

--- Parcial Programación - N°2/scenes/levels/Spikes.gd
+++ Parcial Programación - N°2/scenes/levels/Spikes.gd
@@ -1,10 +1,6 @@
 extends Node2D
 
-export(String) var level_scene = ""
-
 func _on_Area2D_body_entered(body):
 	Globals.monedas = 0
 	if body.get_name() == "car":
-		var err = get_tree().change_scene(level_scene)
-		if err != OK:
-			pass # Replace with function body.
+		get_tree().reload_current_scene()
