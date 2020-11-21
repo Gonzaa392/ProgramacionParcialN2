935dbef8bc234cf3e9fb75928f254c6ed7b842b8
Federico <fededelgado2001@gmail.com>
2020-11-21 00:31:41 -0300

    0.9
    Store añadido, boton añadido al menú, correción del botón "back"

---
+++ Parcial Programación - N°2/Scenes/Menu/Shop.gd
@@ -0,0 +1,24 @@
+extends TextureButton
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Shop_pressed():
+		var err = get_tree().change_scene("res://Scenes/Menu/Store.tscn")
+		if err != OK:
+			print("Error al cambiar de Escena")
+			pass
+
