67c6f3673dfe1b19ebfe266d4563148b06b982ce
Luka <lukafranco2000@hotmail.com>
2020-11-21 00:05:16 -0300

    sistema d skins implementado pero sin la tienda

--- Parcial Programación - N°2/Globals/Globals.gd
+++ Parcial Programación - N°2/Globals/Globals.gd
@@ -5,7 +5,17 @@ var last_time = 0
 var best_time = 0
 var current_time = 0
 
+var sprites = [preload("res://PNG/Car_Blue.png"),preload("res://PNG/Car_Red.png"),preload("res://PNG/Car_Yellow.png")]
+var current_skin = 2
+
 var current_checkpoint = null
+
+func start_level():
+	if get_node_or_null("/root/Main/car/Sprite") != null: #check por seguridad
+		get_node_or_null("/root/Main/car/Sprite").texture = sprites[current_skin]
+	else:
+		print(get_tree().get_root())
+		#car.Sprite.texture = sprites[current_skin]
 
 func reset_level(car):
 	if current_checkpoint != null:
